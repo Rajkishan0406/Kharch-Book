00b44e1d00888c258e5af57c5f0338dc5938c0a2
Rajkishan0406 <rajkishan8298328476@gmail.com>
2021-05-27 11:01:01 -0400

    Ui changes

--- app/src/main/java/com/example/kharchbook/MainActivity.kt
+++ app/src/main/java/com/example/kharchbook/MainActivity.kt
@@ -1,16 +1,41 @@
 package com.example.kharchbook
 
+import android.content.Intent
+import android.hardware.biometrics.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import android.hardware.biometrics.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+import android.hardware.biometrics.BiometricPrompt
+import android.media.audiofx.BassBoost
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings.ACTION_BIOMETRIC_ENROLL
+import android.provider.Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED
+import android.util.Log
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.biometric.BiometricManager
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentTransaction
+import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    lateinit var frame : FrameLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setFragment(SpendFragment())
+
+        frame = findViewById(R.id.main_frame)
+
+        executor = ContextCompat.getMainExecutor(this)
+
+                        setFragment(SpendFragment())
+
+
 
     }
 
